0958adf9f58fad661b6bcdf06bf3adeb11c74543
GloriousLhm <1814003293@qq.com>
2017-10-31 23:53:36 +0800

    用户页面渲染模板引擎部分完成

--- public/back/js/user.js
+++ public/back/js/user.js
@@ -1,43 +1,74 @@
+/**
+ * Created by HUCC on 2017/10/29.
+ */
+
 $(function () {
- // 当前页码
- var currentPage = 1;
- // 显示多少行数据
- var pageSize = 8;
+  //发送ajax请求，获取后台的数据
+  var currentPage = 1;
+  var pageSize = 10;
+  //去后台获取数据，拿的currentPage页的数据
+  function render() {
+    $.ajax({
+      type: "get",
+      url: "/user/queryUser",
+      data: {
+        page: currentPage,
+        pageSize: pageSize
+      },
+      success: function (data) {
+        var html = template("tpl", data);
+        $("tbody").html(html);
 
 
+        //分页功能
+        $("#paginator").bootstrapPaginator({
+          bootstrapMajorVersion: 3, //指定bootstrap的版本
+          currentPage: currentPage, //指定了当前是第几页
+          size: "small",
+          totalPages: Math.ceil(data.total / pageSize),
+          onPageClicked: function (event, originalEvent, type, page) {
+            //为按钮绑定点击事件 page:当前点击的按钮值
+            currentPage = page;
+            render();
+          }
 
- function render() {
-  $.ajax({
-   type: "get",
-   url: "/user/queryUser",
-   data: {
-    page: currentPage,
-    pageSize: pageSize
-   },
-   success: function (data) {
-    console.log(data);
-    var html = template("tpl", data);
-    $("tbody").html(html);
-   }
+        });
+
+      }
+    });
+  }
+  render();
+
+  // 点击启用或者禁用按钮,弹出模态框
+  // 这里的按钮都是动太渲染出来的，所以需事件委托
+
+  $("tbody").on("click", ".btn", function () {
+    $("#userModal").modal("show");
+    var id = $(this).parent().data("id");
+    var isDelete = $(this).parent().data("isDelete");
+    isDelete = isDelete === 1 ? 0 : 1;
+
+    // 因为按钮里面的内容是动态渲染出来的，所以要在注册事件之前解绑之前的事件
+    $(".btn_confirm").off().on("click", function () {
+      // 发送ajax请求
+      $.ajax({
+        type: "post",
+        url: "/user/updateUser",
+        data: {
+          id: id,
+          isDelete: isDelete
+        },
+        success: function (data) {
+          if (data.success) {
+            // 关闭模态框
+            $("#userModal").modal("hide");
+            render();
+          }
+        }
+      });
+    });
   });
 
-  // 分页功能
-  $("#paginator").bootstrapPaginator({
-   bootstrapMajorVersion: 3, //指定bsp的版本
-   currentPage: currentPage,
-   size: "small", //分页栏的大小
-   totalPages: Math.ceil(data.total / pageSize), //总共能分成多页
-   onPageClicked: function (event, originalEnevent, type, page) {
-    // 为按钮添加点击事件 page :当前点击的按扭值
-    // 点击了谁就让当前页第一个序号变为点击的那个页码
-    currentPage = page;
-    render();
-   }
-  })
- }
 
 
-
-
-
-})
+});
